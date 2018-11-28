34e962b72ae095f558f8115474d04f1f388bbfbb
David Varney <david.varney@movehq.com>
2018-11-28 16:37:03 -0500

    Adds some output for Billable column
    Changes to be committed:
    	modified:   bookmarklets/movehq/timeassist.js

--- bookmarklets/movehq/timeassist.js
+++ bookmarklets/movehq/timeassist.js
@@ -3,3 +3,16 @@ var current_url = window.location.host;
 var uri = window.location.pathname;
 var params = window.location.search.substring(1);
 alert(current_url);
+var filtered_content = [];
+switch (current_url) {
+    case 'timeassist.movehq.com':
+        // Billable Column
+        filtered_content.push({column_name: 'Billable?', field_name: 'Billable'});
+        //
+        break;
+    default:
+        // do nothing
+        break;
+}
+
+alert(filtered_content);
