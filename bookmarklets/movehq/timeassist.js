10ba5b2948f20bff0707fd1166e93cd2b3464335
David Varney <david.varney@movehq.com>
2018-11-29 15:16:37 -0500

    Making sure we skip the blank options

--- bookmarklets/movehq/timeassist.js
+++ bookmarklets/movehq/timeassist.js
@@ -34,13 +34,17 @@ if (filtered_content.length > 0) {
             case 'Projects':
             case 'Customers':
                 jQuery('select[name^="' + value.name + '"] option').each(function () {
+                    // Skip the blank options
+                    if (jQuery(this).val() == '') {
+                        return true;
+                    }
                     var select_option = this;
                     var found_it = 0;
                     jQuery.each(value.filtered_options, function (option_index, option_value) {
-//console.log("Field: " + value.name);
-//console.log("Option Value: " + jQuery(select_option).val());
-//console.log("Searching For: " + option_value);
-//console.log("Found?: " + jQuery(select_option).val().indexOf(option_value));
+                        //console.log("Field: " + value.name);
+                        //console.log("Option Value: " + jQuery(select_option).val());
+                        //console.log("Searching For: " + option_value);
+                        //console.log("Found?: " + jQuery(select_option).val().indexOf(option_value));
                         if (jQuery(select_option).val().indexOf(option_value) >= 0) {
                             found_it++;
                         }
