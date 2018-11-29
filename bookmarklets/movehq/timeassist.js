677b2fe9360dfe592e1388f96e495c768d8e2d58
David Varney <david.varney@movehq.com>
2018-11-29 14:20:23 -0500

    Adds logic for filtering and or setting the value of the 4 target fields. Also, starts to add logic for modifying the form values.

--- bookmarklets/movehq/timeassist.js
+++ bookmarklets/movehq/timeassist.js
@@ -7,8 +7,13 @@ var filtered_content = [];
 switch (current_url) {
     case 'timeassist.movehq.com':
         // Billable Column
-        filtered_content.push({column_name: 'Billable?', field_name: 'Billable'});
-        //
+        filtered_content.push({value: false, name: 'Billable', filtered_options: false});
+        // Company Column
+        filtered_content.push({value: 'MoveHQ', name: 'Companies', filtered_options: false});
+        // Project Column
+        filtered_content.push({value: '', name: 'Projects', filtered_options: ['MoveHQ', 'MoveIQ', 'Sirva', 'SIRVA', 'Windfall']});
+        // Customer Column
+        filtered_content.push({value: '', name: 'Customers', filtered_options: ['Move HQ', 'SIRVA']});
         break;
     default:
         // do nothing
@@ -16,3 +21,22 @@ switch (current_url) {
 }
 
 console.log(filtered_content);
+
+if (filtered_content.length > 0) {
+    jQuery.each(filtered_content, function (index, value) {
+        switch (value.name) {
+            case 'Billable':
+                jQuery('input[name^="' + value.name + '"]').prop("checked", value.value);
+                break;
+            case 'Companies':
+                jQuery('select[name^="' + value.name + '"] option:contains("' + value.value + '")').prop("selected", true);
+                break;
+            case 'Projects':
+
+                break;
+            case 'Customers':
+
+                break;
+        }
+    });
+}
